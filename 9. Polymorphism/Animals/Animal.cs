fd064398f3beb71418b82b36441af91ceda5b7cf
Yoanna Genchev <joanna.stoycheva.bg@gmail.com>
2025-03-21 14:49:02 +0200

    add

---
+++ 9. Polymorphism/Animals/Animal.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animals
+{
+    public class Animal
+    {
+        protected Animal(string name, string favouriteFood)
+        {
+            Name = name;
+            FavouriteFood = favouriteFood;
+        }
+
+        public string Name { get; protected set; }
+        public string FavouriteFood { get; protected set; }
+
+        public virtual string ExplainSelf()
+        {
+            return $"I am {this.Name} and my favourite food is {this.FavouriteFood}";
+        }
+    }
+}
