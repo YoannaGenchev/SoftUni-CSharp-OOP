8b72ba5a8ae9baaf2fbecd1944f71bc3cbbd2176
Yoanna Genchev <joanna.stoycheva.bg@gmail.com>
2025-03-21 14:38:39 +0200

    add

---
+++ 3. Inheritance/04.RandomList/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace CustomRandomList
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            RandomList list = new RandomList { "first", "second", "third", "hello", "world"};
+
+            while (list.Count > 0)
+            {
+                Console.WriteLine(list.RandomString());
+            }
+        }
+    }
+}
---
+++ 3. Inheritance/04.RandomList/RandomList.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomRandomList
+{
+    public class RandomList : List<string>
+    {
+        public string RandomString()
+        {
+            int index = Random.Shared.Next(this.Count);
+            string str = this[index];
+            this.RemoveAt(index);
+            return str;
+        }
+    }
+}
